cdbce584afc91cb2313fce977dfcccd7a8de0389
skibadooCunanan <cunanan.dt@obmontessori.edu.ph>
2026-09-21 08:34:58 +0800

    j

---
+++ main.py
@@ -0,0 +1,25 @@
+from pyscript import display, document
+
+# a = 'j'
+# b = 9
+
+# display(a * b)
+
+# indexing / offset [] 0
+# name = ["information", "technology"]
+# display(name[-1])
+
+# longest_word = ['pneumonoultramicroscopicsilicovolcanoconiosis']
+# display(len(longest_word))
+
+# a = "Monday, Tuesday, Wednesday"
+
+# display(a.split(','))
+# display(type(a.split(',')))
+
+# a = ['Monday, Tuesday, Wednesday']
+# b = "I hate the following days:"
+# display(b + ','.join(a))
+
+fave = "I like liver, tomatoes, and parsley"
+display(fave.replace("liver", 'salmon'))
